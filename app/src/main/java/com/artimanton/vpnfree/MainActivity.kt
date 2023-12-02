0968d32054798ffa161edd4f69f98f0e372db5a0
artim-anton <1Rimola0>
2023-12-02 16:37:46 +0300

    added open moreServers, settings

--- app/src/main/java/com/artimanton/vpnfree/MainActivity.kt
+++ app/src/main/java/com/artimanton/vpnfree/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.artimanton.vpnfree
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,6 +13,16 @@ class MainActivity : AppCompatActivity() {
         val mainBinding: ActivityMainBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(mainBinding.root)
 
+        mainBinding.moreServers.setOnClickListener {
+            val activityOptions = ActivityOptions.makeCustomAnimation(this@MainActivity, 0, 0)
+            startActivity(Intent(this@MainActivity, ServerListActivity::class.java), activityOptions.toBundle())
+            finish()
+        }
 
+        mainBinding.settings.setOnClickListener {
+            val activityOptions = ActivityOptions.makeCustomAnimation(this@MainActivity, 0, 0)
+            startActivity(Intent(this@MainActivity, SettingActivity::class.java), activityOptions.toBundle())
+            finish()
+        }
     }
 }
--- app/src/main/java/com/artimanton/vpnfree/ServerListActivity.kt
+++ app/src/main/java/com/artimanton/vpnfree/ServerListActivity.kt
@@ -3,7 +3,7 @@ package com.artimanton.vpnfree
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ServerList_Activity : AppCompatActivity() {
+class ServerListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_server_list)
